9ec73ea3aebc7979dbde4a8ad92f869f21b9596c
thami911 <thamimncwabe@gmail.com>
2023-03-16 06:52:01 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,17 +17,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 	else
 	{ return (malloc(new_size)); }
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (0);
 	}
-
 	relloc = malloc(new_size);
 	if (relloc == NULL)
 		return (0);
-
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
 		*(relloc + i) = clone[i];
